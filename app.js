51e016e52d17bc095bf7c65a7e6449a9926033a6
YonatanCorales <yonatancoralesb@gmail.com>
2025-01-27 20:42:54 -0500

    Version Final Juego Secreto

---
+++ app.js
@@ -0,0 +1,73 @@
+
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(intentos);
+    if(numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos ===1) ? 'vez' : 'veces'}` );
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        // El usuario no acerto
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El numero secreto es menor')
+        }else{
+            asignarTextoElemento('p', 'El numero secreto es mayor')
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value='';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    
+    //Si ya sorteamos todos los numeros
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p','Ya se asignaron todos los intentos posibles');
+    }else{
+        // //Si el numero generado esta incluido en la lista:
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        }else{
+            listaNumerosSorteados.push(numeroGenerado);
+        }
+    }
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego Del Numero Secreto');
+    asignarTextoElemento('p',`Ingresa un numero del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    //limpiar la caja
+    limpiarCaja();
+    //indicar mendaje de intervalo de numeros
+    //Generar el numero aleatorio
+    //inicializar el numero de intentos
+    condicionesIniciales();
+    //desactivar el boton nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled',true)
+    
+}
+
+condicionesIniciales();
+
